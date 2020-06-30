00d43b2bb18c860dc698312461a56e443e5d36ec
IliaBnn <ilya.bnn@gmail.com>
2020-06-30 15:38:19 +0400

    6.0.2. Заполнение массива. [#313236]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -6,5 +6,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Aleksey";
+        names[1] = "Marta";
+        names[2] = "Polina";
+        names[3] = "Ilia";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
