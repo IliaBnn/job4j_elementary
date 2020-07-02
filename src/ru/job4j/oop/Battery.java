538f86c582ea92663dc231302b0a8642211db991
IliaBnn <ilya.bnn@gmail.com>
2020-07-02 15:51:13 +0400

    1.7. Состояние объекта [#313395]

--- src/ru/job4j/oop/Battery.java
+++ src/ru/job4j/oop/Battery.java
@@ -4,8 +4,24 @@ public class Battery {
 
     private int load;
 
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
     public void exchange(Battery another) {
         another.load += this.load;
         this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery source = new Battery(50);
+        Battery battery = new Battery(50);
+        battery.exchange(source);
+        System.out.println("Заряд источника: " + source.getLoad() + "\n" +
+                "Заряд получателя: " + battery.getLoad());
     }
 }
